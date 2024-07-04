b87359f579c1be7f3ff634bb99109e00b02f78ed
Dhruv Agarwal <135869308+DhruvX123@users.noreply.github.com>
2024-07-05 01:52:50 +0530

    Delete Task_Manager/src/main/java/com/project/task_manager/Repositary/RemainderRepo.java

--- Task_Manager/src/main/java/com/project/task_manager/Repositary/RemainderRepo.java
+++
@@ -1,7 +0,0 @@
-package com.project.task_manager.Repositary;
-
-import com.project.task_manager.Entity.Remainder;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RemainderRepo extends JpaRepository<Remainder, Long> {
-}
