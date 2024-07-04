86dd6ab536d70c67e3fc9caf544abc0e909a51e4
Dhruv Agarwal <135869308+DhruvX123@users.noreply.github.com>
2024-07-05 01:51:29 +0530

    Delete Task_Manager/src/main/java/com/project/task_manager/Repositary/CategoryRepo.java

--- Task_Manager/src/main/java/com/project/task_manager/Repositary/CategoryRepo.java
+++
@@ -1,7 +0,0 @@
-package com.project.task_manager.Repositary;
-
-import com.project.task_manager.Entity.Category;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CategoryRepo extends JpaRepository<Category, Long> {
-}
